ae5f77dc448d6c79bb58dd4bdd49dda655132ade
matthewf01 <matthewfugel@gmail.com>
2016-04-08 12:40:20 -0400

    Create sensor.py

---
+++ sensor.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+#modified script from http://www.raspberrypi-spy.co.uk/2012/12/ultrasonic-distance-measurement-using-python-part-1/
+
+#Import required Python libraries
+import time
+import RPi.GPIO as GPIO
+
+#GPIO setup
+GPIO.setmode(GPIO.BCM)
+GPIO_TRIGGER = 23
+GPIO_ECHO = 24
+
+# Set pins as output and input
+GPIO.setup(GPIO_TRIGGER,GPIO.OUT)  # Trigger
+GPIO.setup(GPIO_ECHO,GPIO.IN)      # Echo
+
+# Set trigger to False (Low)
+GPIO.output(GPIO_TRIGGER, False)
+
+# Allow module to settle
+time.sleep(0.5)
+
+print "Starting ultrasonic distance measure"
+
+# Send 10us pulse to trigger
+GPIO.output(GPIO_TRIGGER, True)
+time.sleep(0.00001)
+GPIO.output(GPIO_TRIGGER, False)
+start = time.time()
+while GPIO.input(GPIO_ECHO)==0:
+  start = time.time()
+
+while GPIO.input(GPIO_ECHO)==1:
+  stop = time.time()
+
+# Calculate pulse length
+elapsed = stop-start
+
+# Distance pulse travelled in that time is time
+# multiplied by the speed of sound (cm/s)
+distance = elapsed * 34000
+
+# That was the distance there and back so halve the value
+distance = distance / 2
+
+print "Distance : %.1f" % distance
+
+# Reset GPIO settings
+GPIO.cleanup()
