8b4d2e28ead980c559ca8f4965afcf47ee5e48b1
matthewf01 <matthewfugel@gmail.com>
2016-04-09 20:29:33 -0400

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -48,31 +48,33 @@ GPIO.output(LED_GREEN,False)
 
 print "Starting ultrasonic distance measure"
 
-# Send 10us pulse to trigger
-GPIO.output(GPIO_TRIGGER, True)
-time.sleep(0.00001)
-GPIO.output(GPIO_TRIGGER, False)
-start = time.time()
-while GPIO.input(GPIO_ECHO)==0:
-  start = time.time()
-  GPIO.output(LED_GREEN,True)
+while True:
+    # Send 10us pulse to trigger 
+    GPIO.output(GPIO_TRIGGER, True)
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+    start = time.time()
+    while GPIO.input(GPIO_ECHO)==0:
+      start = time.time()
+      GPIO.output(LED_GREEN,True)
 
-while GPIO.input(GPIO_ECHO)==1:
-  stop = time.time()
-  GPIO.output(LED_GREEN,False)
-  GPIO.output(LED_RED, True)
+    while GPIO.input(GPIO_ECHO)==1:
+      stop = time.time()
+      GPIO.output(LED_GREEN,False)
+      GPIO.output(LED_RED, True)
 
-# Calculate pulse length
-elapsed = stop-start
+    # Calculate pulse length
+    elapsed = stop-start
 
-# Distance pulse travelled in that time is time
-# multiplied by the speed of sound (cm/s)
-distance = elapsed * 34000
+    # Distance pulse travelled in that time is time
+    # multiplied by the speed of sound (cm/s)
+    distance = elapsed * 34000
 
-# That was the distance there and back so halve the value
-distance = distance / 2
+    # That was the distance there and back so halve the value
+    distance = distance / 2
 
-print "Distance : %.1f" % distance
+    print "Distance : %.1f" % distance
+    time.sleep(3)
 
 # Reset GPIO settings
 lightsoff()
