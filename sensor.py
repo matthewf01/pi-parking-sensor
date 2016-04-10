9e2b4df999e1d834a2d53bbae2f27b8920d2b999
matthewf01 <matthewfugel@gmail.com>
2016-04-09 22:49:13 -0400

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -118,7 +118,7 @@ try:
       fastblink(LED_RED)
       print ("Inside warn range")
     #elif distance in range(innerlimit,outerlimit):
-    elif distance > innerlimit and < outerlimit:
+    elif distance => innerlimit distance and <= outerlimit:
       lightson()
       print("Stop, perfect")
     elif distance < dist_stop - dist_stop_tolerance:
